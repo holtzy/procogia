b7781e7be36c204e8829107a1ee2ad874393279e
Holtz Yan <yan.holtz@datadoghq.com>
2025-03-18 21:07:30 +0100

    build skill categories

---
+++ src/data/skill-categories.ts
@@ -0,0 +1,52 @@
+export const skillCategories = {
+    "Cloud Computing & DevOps": [
+        "AWS (Amazon Web Services)", "AWS CloudFormation", "AWS Redshift", "Azure",
+        "Azure AI and Machine Learning services", "Azure Blob storage", "Azure Data Explorer (ADX)",
+        "Azure Data Factory", "Azure Data Lake", "Azure DevOps", "Azure Synapse Analytics",
+        "Google Cloud Platforms", "Google Cloud AI and Machine Learning services", "Databricks",
+        "Kubernetes", "Terraform", "Docker", "Ansible", "Chef", "Puppet", "Istio", "Linkerd",
+        "CircleCI", "GitHub", "GitLab CI", "Jenkins"
+    ],
+    "Data Engineering & Big Data": [
+        "Apache Airflow", "Apache Flink", "Apache Kafka", "Apache NiFi", "Apache Spark",
+        "Hadoop", "ELK Stack (Elasticsearch, Logstash, Kibana)", "OpenSearch", "Snowflake",
+        "Cassandra (NoSQL)", "Cosmos DB", "MongoDB (NoSQL)", "Oracle Database", "PostgreSQL",
+        "MySQL", "SQL Server", "Redis"
+    ],
+    "Machine Learning & AI": [
+        "TensorFlow", "PyTorch", "Keras", "LightGBM", "XGBoost", "scikit-learn", "spaCy",
+        "NLTK", "Hugging Face Transformers", "OpenCV", "StyleGAN", "CycleGAN", "VQ-VAE",
+        "BERT", "DALL-E", "LangChain"
+    ],
+    "Data Science & Analytics": [
+        "NumPy", "Pandas", "dplyr (R)", "tidyverse", "ggplot2", "Seaborn", "Matplotlib",
+        "Plotly", "Power BI", "Tableau", "Qlik Sense", "QlikView", "Looker", "ThoughtSpot",
+        "Quick Sight", "Google Analytics", "Adobe Analytics"
+    ],
+    "Software Development & Programming": [
+        "Python", "R Programming", "Java", "JavaScript", "Bash", "Shell Scripting", "SQL",
+        "Scala", "HTML", "CSS", "jQuery", "Package Development (R)", "RapidMiner"
+    ],
+    "MLOps & Model Deployment": [
+        "MLOps dashboards (mlflow, Weights & Biases)", "Streamlit", "Shiny", "Plumber",
+        "FastAPI", "Model Deployment"
+    ],
+    "Data Modeling & ETL": [
+        "ER Modeling", "Dimensional Modeling", "Talend", "Informatica", "dbt", "Celonis"
+    ],
+    "Monitoring & Logging": [
+        "Grafana", "Prometheus", "Splunk", "Ganglia"
+    ],
+    "Version Control & Collaboration": [
+        "Git", "GitHub", "GitLab CI", "Bitbucket"
+    ],
+    "Business Intelligence & Reporting": [
+        "Domo", "MetaBase", "Microsoft Excel", "Collibra"
+    ],
+    "Statistical Analysis & Bioinformatics": [
+        "Biostatistics", "Bioconductor"
+    ],
+    "Other Tools & Technologies": [
+        "IDEA", "ThoughtSpot", "Years of Experience"
+    ]
+}
