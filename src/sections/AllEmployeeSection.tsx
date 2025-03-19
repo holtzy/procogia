2daf58f4d4361d93c947cff818ecc0daaaae73cb
Holtz Yan <yan.holtz@datadoghq.com>
2025-03-19 13:21:15 +0100

    use ShadCnUI

--- src/sections/AllEmployeeSection.tsx
+++ src/sections/AllEmployeeSection.tsx
@@ -2,6 +2,13 @@ import { useState } from "react";
 import { employeesData } from "../data/data";
 import { AXIS_CONFIG, COLORS } from "../utils";
 import { Radar } from "../dataviz/radar-chart/Radar";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export const AllEmployeeSection = () => {
   const allGroups = employeesData.map((d) => d.name);
@@ -14,17 +21,22 @@ export const AllEmployeeSection = () => {
   const groupRadarData = employeesData[groupId];
 
   return (
-    <div>
-      <div>
-        {allGroups.map((group) => {
-          //   const isSelected = group === selectedGroup;
-          return (
-            <button key={group} onClick={() => setSelectedGroup(group)}>
-              {group}
-            </button>
-          );
-        })}
-      </div>
+    <div className="mx-auto w-full">
+      <Select value={selectedGroup} onValueChange={setSelectedGroup}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          {allGroups.map((group) => {
+            //   const isSelected = group === selectedGroup;
+            return (
+              <SelectItem key={group} value={group}>
+                {group}
+              </SelectItem>
+            );
+          })}
+        </SelectContent>
+      </Select>
 
       <Radar
         data={groupRadarData}
